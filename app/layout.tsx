97b1a27015954ffcf4b866a004a1f78ce93f60fa
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-04-26 12:07:14 -0700

    Add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,12 @@ const satoshi = localFont({
   src: [
     {
       path: '../public/fonts/Satoshi/Satoshi-Variable.woff2',
+      weight: '300 900',
       style: 'normal',
     },
     {
       path: '../public/fonts/Satoshi/Satoshi-VariableItalic.woff2',
+      weight: '300 900',
       style: 'italic',
     },
   ],
---
+++ app/styles/page.tsx
@@ -0,0 +1,12 @@
+export default function Page(){
+    return(
+        <>
+            <h1>Header 1</h1>
+            <h2>Header 2</h2>
+            <h3>Header 3</h3>
+            <p>This is a paragraph</p>
+            <p className="info">This is information</p>
+            <p className="pull-quote">This is a quote</p>
+        </>
+    )
+}
