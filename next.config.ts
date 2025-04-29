8a43e8bed37f3209011b35b19d2374822084f694
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-04-29 16:54:04 -0700

    Remove vercel.json and add vercel redirect to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "goldengateinstitute.org",
+          },
+        ],
+        destination: "https://www.goldengateinstitute.org",
+        permanent: true,
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
