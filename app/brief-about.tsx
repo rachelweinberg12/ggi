76971dd11beed7beab4b19131f16f9647fa2b081
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-04-30 11:33:41 -0700

    Update brief about section

--- app/brief-about.tsx
+++ app/brief-about.tsx
@@ -9,25 +9,44 @@ export function BriefAbout() {
       <h2>About us</h2>
       <Col className="mt-3 gap-2">
         <p>
-          We envision a future where institutions and experts across domains
-          collaborate with urgency and humility to meet the challenges of the AI
-          transition, promoting security, democracy, and shared prosperity.
+          We believe that AI will be a transformative technology, with the
+          potential for enormous upside and downside.
         </p>
         <p>
-          <b>Attendees:</b> We bring in participants from frontier AI labs,
-          startups, think tanks, government, academia, civil society, and media.
-          This event is invite-only. Sign up here to be notified when our
-          application opens in June.
+          Developments in AI have been moving so quickly that expert consensus
+          has not had time to emerge. When the experts are talking past one
+          another, decision-makers can’t make sense of what’s going on. Without
+          understanding, they’ll miss opportunities and stumble into disasters.
         </p>
         <p>
-          <b>Topics:</b> Our content will get at questions like — How can we
-          unlock the benefits of AI for scientific discovery, institutional
-          function, and global prosperity? How might AI concentrate or
-          distribute power? What are the risks posed by current and future AI
-          systems? How quickly will AI systems become more powerful? What impact
-          will they have on the job market and economic growth? What
-          implications does AI have for national security and global balance of
-          power?
+          We aim to support leaders to meet the challenges of the AI transition
+          by bridging disciplines, convening experts, and publishing accessible
+          analysis of AI’s toughest questions.
+        </p>
+        <p>We focus on four broad topics:</p>
+        <ul>
+          <li>
+            <b>Timelines & Capabilities:</b> How quickly will AI development
+            advance?
+            <li>
+              <b>Economic Impacts:</b> How quickly will AI be adopted, and what
+              impact will this have on the economy? How can we ensure AI creates
+              broad-based economic benefits?
+            </li>
+            <li>
+              <b>Institutional Adaptation:</b> How must democratic and other key
+              institutions adapt to the challenges and opportunities that AI
+              brings?
+            </li>
+            <li>
+              <b>Realizing Benefits:</b> What can we do to unlock and facilitate
+              adoption of beneficial uses of AI?
+            </li>
+          </li>
+        </ul>
+        <p>
+          Our founding team includes Taren Stinebrickner-Kauffman, Steve Newman,
+          and Rachel Weinberg.
         </p>
       </Col>
     </div>
--- app/the-curve-spotlight.tsx
+++ app/the-curve-spotlight.tsx
@@ -26,15 +26,15 @@ export function TheCurveSpotlight() {
       <h1>The Curve</h1>
       <h5 className="mt-1">October 3-5, 2025 | SF & Berkeley</h5>
       <h2 className="mt-3">
-        A gathering for leading thinkers, builders, advocates, and
-        decision-makers to make sense of the AI big picture.
+        A conference for thinkers, builders, and leaders to tackle the AI big
+        picture.
       </h2>
       <Col className="mt-8 gap-2">
         <p>
-          <b>Purpose:</b> No single group has all the answers or execution power
-          to make the best of transformative AI. The Curve creates a space for
-          the people creating and steering this technology to share knowledge,
-          explore disagreements, and coordinate on strategy.
+          <b>Purpose:</b> The Curve creates a space for the people creating and
+          steering AI to share knowledge, explore disagreements, and coordinate
+          on strategy. No single cohort or discipline has all that it takes to
+          make the best of transformative AI. We aim to help them work together.
         </p>
         <p>
           <b>Attendees:</b> We bring in participants from frontier AI labs,
