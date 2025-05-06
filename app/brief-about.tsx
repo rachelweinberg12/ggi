d1d724931b11c2d8b1ee5694087d89d9518fb3ab
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-05-05 21:50:12 -0700

    Fix padding on text sections

--- app/brief-about.tsx
+++ app/brief-about.tsx
@@ -6,28 +6,31 @@ export function BriefAbout() {
       className="section-padding border-y border-black theme-sand"
       id="about"
     >
-      <h2>About us</h2>
-      <Col className="mt-3 gap-2">
-        <p>
-          We believe that AI will be a transformative technology, with the
-          potential for enormous upside and downside.
-        </p>
-        <p>
-          Developments in AI have been moving so quickly that expert consensus
-          has not had time to emerge. When the experts are talking past one
-          another, decision-makers can’t make sense of what’s going on. Without
-          understanding, they’ll miss opportunities and stumble into disasters.
-        </p>
-        <p>
-          We aim to support leaders to meet the challenges of the AI transition
-          by bridging disciplines, convening experts, and publishing accessible
-          analysis of AI’s toughest questions.
-        </p>
-        <p>We focus on four broad topics:</p>
-        <ul>
-          <li>
-            <b>Timelines & Capabilities:</b> How quickly will AI development
-            advance?
+      <div className="text-width-pos">
+        <h2>About us</h2>
+        <Col className="mt-3 gap-2">
+          <p>
+            We believe that AI will be a transformative technology, with the
+            potential for enormous upside and downside.
+          </p>
+          <p>
+            Developments in AI have been moving so quickly that expert consensus
+            has not had time to emerge. When the experts are talking past one
+            another, decision-makers can’t make sense of what’s going on.
+            Without understanding, they’ll miss opportunities and stumble into
+            disasters.
+          </p>
+          <p>
+            We aim to support leaders to meet the challenges of the AI
+            transition by bridging disciplines, convening experts, and
+            publishing accessible analysis of AI’s toughest questions.
+          </p>
+          <p>We focus on four broad topics:</p>
+          <ul>
+            <li>
+              <b>Timelines & Capabilities:</b> How quickly will AI development
+              advance?
+            </li>
             <li>
               <b>Economic Impacts:</b> How quickly will AI be adopted, and what
               impact will this have on the economy? How can we ensure AI creates
@@ -42,13 +45,13 @@ export function BriefAbout() {
               <b>Realizing Benefits:</b> What can we do to unlock and facilitate
               adoption of beneficial uses of AI?
             </li>
-          </li>
-        </ul>
-        <p>
-          Our founding team consists of Taren Stinebrickner-Kauffman, Steve
-          Newman, and Rachel Weinberg.
-        </p>
-      </Col>
+          </ul>
+          <p>
+            Our founding team consists of Taren Stinebrickner-Kauffman, Steve
+            Newman, and Rachel Weinberg.
+          </p>
+        </Col>
+      </div>
     </div>
   );
 }
--- app/the-curve-spotlight.tsx
+++ app/the-curve-spotlight.tsx
@@ -23,41 +23,44 @@ export function TheCurveSpotlight() {
       className="section-padding border-y border-black theme-teal"
       id="the-curve"
     >
-      <h1>The Curve</h1>
-      <h5 className="mt-1">October 3-5, 2025 | SF & Berkeley</h5>
-      <h2 className="mt-3">
-        A conference for thinkers, builders, and leaders to tackle the AI big
-        picture.
-      </h2>
-      <Col className="mt-8 gap-2">
-        <p>
-          <b>Purpose:</b> The Curve creates a space for the people creating and
-          steering AI to share knowledge, explore disagreements, and coordinate
-          on strategy. No single cohort or discipline has all that it takes to
-          make the best of transformative AI. We aim to help them work together.
-        </p>
-        <p>
-          <b>Attendees:</b> We bring in participants from frontier AI labs,
-          startups, think tanks, government, academia, civil society, and media.
-          This event is invite-only. Sign up here to be notified when our
-          application opens in June.
-        </p>
-        <p>
-          <b>Topics:</b> Our content will get at questions like — How can we
-          unlock the benefits of AI for scientific discovery, institutional
-          function, and global prosperity? How might AI concentrate or
-          distribute power? What are the risks posed by current and future AI
-          systems? How quickly will AI systems become more powerful? What impact
-          will they have on the job market and economic growth? What
-          implications does AI have for national security and global balance of
-          power?
-        </p>
-      </Col>
-      <Col className="y-gap x-gap sm:flex-row mt-8">
-        {testimonials.map((t) => (
-          <Testimonial quote={t.quote} name={t.name} key={t.name} />
-        ))}
-      </Col>
+      <div className="text-width-pos">
+        <h1>The Curve</h1>
+        <h5 className="mt-1">October 3-5, 2025 | SF & Berkeley</h5>
+        <h2 className="mt-3">
+          A conference for thinkers, builders, and leaders to tackle the AI big
+          picture.
+        </h2>
+        <Col className="mt-8 gap-2">
+          <p>
+            <b>Purpose:</b> The Curve creates a space for the people creating
+            and steering AI to share knowledge, explore disagreements, and
+            coordinate on strategy. No single cohort or discipline has all that
+            it takes to make the best of transformative AI. We aim to help them
+            work together.
+          </p>
+          <p>
+            <b>Attendees:</b> We bring in participants from frontier AI labs,
+            startups, think tanks, government, academia, civil society, and
+            media. This event is invite-only. Sign up here to be notified when
+            our application opens in June.
+          </p>
+          <p>
+            <b>Topics:</b> Our content will get at questions like — How can we
+            unlock the benefits of AI for scientific discovery, institutional
+            function, and global prosperity? How might AI concentrate or
+            distribute power? What are the risks posed by current and future AI
+            systems? How quickly will AI systems become more powerful? What
+            impact will they have on the job market and economic growth? What
+            implications does AI have for national security and global balance
+            of power?
+          </p>
+        </Col>
+        <Col className="y-gap x-gap sm:flex-row mt-8">
+          {testimonials.map((t) => (
+            <Testimonial quote={t.quote} name={t.name} key={t.name} />
+          ))}
+        </Col>
+      </div>
     </div>
   );
 }
