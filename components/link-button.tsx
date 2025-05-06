1809b6f90c063c6c2aa200ea51a05d321741ed83
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-05-06 10:26:46 -0700

    Update link button sizing

--- components/link-button.tsx
+++ components/link-button.tsx
@@ -5,7 +5,9 @@ export function LinkButton(props: { href: string; title: string }) {
   const { href, title } = props;
   return (
     <Link href={href} className="flex items-center gap-1 group">
-      <span className="text-nowrap group-hover:underline">{title}</span>{" "}
+      <span className="text-nowrap group-hover:underline md:text-lg">
+        {title}
+      </span>{" "}
       <ArrowLongRightIcon className="w-4 h-4 relative top-0.5" />
     </Link>
   );
