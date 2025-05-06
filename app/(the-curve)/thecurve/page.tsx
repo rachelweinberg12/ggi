6df5daf43ea912f9114fee6aebbbe8650fa5de3b
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-05-06 12:59:31 -0700

    Refine The Curve header

--- app/(the-curve)/thecurve/page.tsx
+++ app/(the-curve)/thecurve/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
 function Hero() {
   return (
     <div className="w-full theme-orange section-padding">
-      <div className="relative h-[40dvw] max-h-[400px] mx-auto max-w-7xl">
-        <div className="px-12">
+      <div className="relative h-[400px] sm:h-[40dvw] sm:max-h-[400px] mx-auto max-w-7xl">
+        <div className="sm:px-12">
           <h1 className="mt-5">The Curve</h1>
           <h5 className="mt-1">October 3-5, 2025 | SF & Berkeley</h5>
           <h2 className="lg:my-12 my-6 w-2/3">
@@ -20,9 +20,9 @@ function Hero() {
             big picture.
           </h2>
         </div>
-        <div className="absolute xl:bottom-12 bottom-0 left-0 w-full h-full flex justify-center items-center">
+        <div className="hidden sm:flex absolute xl:bottom-12 bottom-0 left-0 w-full h-full justify-center items-center">
           <Image
-            className="object-contain sm:aspect-16/9 aspect-2/3 text-white"
+            className="object-contain text-white"
             src="/WideCurve.svg"
             alt="Curve"
             width={2000}
