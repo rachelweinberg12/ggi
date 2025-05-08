ccbb49f167cc5a440311849c4503c8704092e8ad
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-05-08 12:06:44 -0700

    Update socials

--- components/socials.tsx
+++ components/socials.tsx
@@ -1,11 +1,33 @@
+import { LINKEDIN_URL, SUBSTACK_URL, X_URL } from "@/utils/constants";
 import { Row } from "./blocks";
 import { SocialIcon } from "react-social-icons";
 export function Socials() {
   return (
-    <Row>
-      <SocialIcon network="x" borderRadius="6px" bgColor="black" />
-      <SocialIcon network="substack" borderRadius="6px" bgColor="black" />
-      <SocialIcon network="linkedin" borderRadius="6px" bgColor="black" />
+    <Row className="gap-4">
+      <SocialIcon
+        network="x"
+        href={X_URL}
+        borderRadius="6px"
+        style={{ width: 32, height: 32 }}
+        bgColor="#141312"
+        target="_blank"
+      />
+      <SocialIcon
+        network="substack"
+        href={SUBSTACK_URL}
+        borderRadius="6px"
+        style={{ width: 32, height: 32 }}
+        bgColor="#141312"
+        target="_blank"
+      />
+      <SocialIcon
+        network="linkedin"
+        href={LINKEDIN_URL}
+        borderRadius="6px"
+        style={{ width: 32, height: 32 }}
+        bgColor="#141312"
+        target="_blank"
+      />
     </Row>
   );
 }
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,7 @@
 export const SUBSTACK_URL = "https://amistrongeryet.substack.com";
+export const LINKEDIN_URL =
+  "https://www.linkedin.com/company/golden-gate-institute-for-ai";
+export const X_URL = "https://x.com/GoldenGateInst";
 export const TYPEFORM_IDS = {
   ANNOUNCEMENTS: "CqwMxK3C",
   TC_NOTIF: "CGFfW1bw", // Deprecated
