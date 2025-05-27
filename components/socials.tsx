c8f83e26ec68837486954dd5b4d1c2705f61520c
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-05-27 16:40:30 -0700

    Add info @ email address to socials in footer

--- components/socials.tsx
+++ components/socials.tsx
@@ -1,9 +1,22 @@
-import { LINKEDIN_URL, SUBSTACK_URL, X_URL } from "@/utils/constants";
+import {
+  EMAIL_ADDRESS,
+  LINKEDIN_URL,
+  SUBSTACK_URL,
+  X_URL,
+} from "@/utils/constants";
 import { Row } from "./blocks";
 import { SocialIcon } from "react-social-icons";
 export function Socials() {
   return (
     <Row className="gap-4">
+      <SocialIcon
+        network="email"
+        href={`mailto:${EMAIL_ADDRESS}`}
+        borderRadius="6px"
+        style={{ width: 32, height: 32 }}
+        bgColor="#141312"
+        target="_blank"
+      />
       <SocialIcon
         network="x"
         href={X_URL}
