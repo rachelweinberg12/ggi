9fefef4025190ec902eca59f19d57199c73af525
rachelweinberg12 <rachel.weinberg12@gmail.com>
2025-06-09 21:29:35 -0700

    Fix padding in cybersecurity equilibrium page

--- app/(main)/cyber-security-report/cybersecurity-equilibrium/page.tsx
+++ app/(main)/cyber-security-report/cybersecurity-equilibrium/page.tsx
@@ -3,12 +3,11 @@ import Link from "next/link";
 import { SUBSTACK_URL } from "@/utils/constants";
 export default function Page() {
   return (
+    <div className="top-section-padding">
       <div className="text-width-pos">
-
         <h2 className="mb-4">The Cybersecurity Equilibrium</h2>
-        <p className="mb-2">
-          lorem ipsum gloria mundi whamma lamma ding dong
-        </p>
+        <p className="mb-2">lorem ipsum gloria mundi whamma lamma ding dong</p>
       </div>
+    </div>
   );
 }
