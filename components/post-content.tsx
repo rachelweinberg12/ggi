71a45069d0b5212a17990891786faf1cdb2ca6c4
Steve Newman <steve@snewman.net>
2025-06-13 14:31:25 -0700

    Style fixes

--- components/post-content.tsx
+++ components/post-content.tsx
@@ -25,15 +25,15 @@ export const PostContent: React.FC<PostContentProps> = ({ content }) => {
         rehypePlugins={[rehypeRaw]}
         components={{
           h1: ({ node, ...props }) => <h3 {...props} className="mt-12" />,
-          h2: ({ node, ...props }) => <h4 {...props} className="mt-12" />,
+          h2: ({ node, ...props }) => <h4 {...props} className="mt-8" />,
           h3: ({ node, ...props }) => (
-            <p {...props} className="font-bold mt-12" />
+            <p {...props} className="font-bold mt-4" />
           ),
           p: ({ node, ...props }) => <p {...props} className="mt-4" />,
           a: ({ node, ...props }) => <a {...props} className="text-link" />,
           blockquote: ({ node, ...props }) => <blockquote {...props} className="pull-quote" />,
           table: ({ node, ...props }) => (
-            <table {...props} className="border-collapse border border-gray-300 [&_td]:border [&_td]:border-gray-300 [&_th]:border [&_th]:border-gray-300" />
+            <table {...props} className="border-collapse border border-gray-300 [&_td]:border [&_td]:border-gray-300 [&_th]:border [&_th]:text-left [&_th]:px-4 [&_th]:py-2 [&_td]:px-4 [&_td]:py-2" />
           ),
         }}
       >
